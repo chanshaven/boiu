1fd968b59aec462fea41fa9535b6d85237fc4d6a
Chuong Luong <luvichbopi@gmail.com>
2026-09-24 11:37:13 +0200

    update card

---
+++ boius/boiu-02/wishes.js
@@ -0,0 +1,109 @@
+/* ==========================================
+   LANTERN WISHES
+========================================== */
+
+export const lanternWishes = [
+
+    {
+        title: "Thôn lành pôn lì ♡",
+
+        message:
+            "Ông nậu nói chiều nào cũng chở em đi chơi kìa, cho em ra đồng hít cít dịt.",
+
+        image: "images/wish-01.jpg"
+        /*
+           Nếu muốn có ảnh:
+
+           image: "images/wish-01.jpg"
+
+           Nếu không:
+        */
+        // image: null
+    },
+
+
+    {
+        title: "Trung thu đầu tiên của em mà em chả biết gì 🌙",
+
+        message:
+            "Bà nậu đau tay nên k thấy ảnh nào riêng với em hết. Em chúc bà nậu khỏi bệnh đau tay + lo âu",
+        image: "images/wish-02.jpg"
+        // image: null
+    },
+
+
+    {
+        title: "Đây rồi đây rồi kho bạc của em đây rồi 🏮",
+
+        message:
+            "Cô 3 là con quỷ!",
+
+        image: "images/wish-03.jpg"
+        // image: null
+    },
+
+
+    {
+        title: "Bầu trời của em ✨",
+
+        message:
+            "Trừ đèn lồng và ánh điện sân bay ra thì mẹ là người mà em yêu nhất",
+
+        // image: null
+        image: "images/wish-04.jpg"
+    },
+
+
+    {
+        title: "Ông lọi nào hay ở chung nhà với mình ♡",
+
+        message:
+            "Thấy cha này hay ở chung nhà với mình, lâu lâu thấy mặt, nhiều hơn mấy người khác, chắc cũng thân thiết lắm đây",
+
+        /*
+        Ví dụ sau này:
+
+        image: "images/wish-05.jpg"
+        */
+
+        // image: null
+        image: "images/wish-05.jpg"
+    },
+
+
+    {
+        title: "Cười đi, nhìn cái gì mà nhìn 🏮",
+
+        message:
+            "Trung thu dủi dẻ nhen",
+
+        // image: null
+        image: "images/final.jpg"
+    }
+
+];
+
+
+/* ==========================================
+   FINAL LETTER
+========================================== */
+
+export const finalLetter = {
+
+    title:
+        "Trung thu dui dẻ ♡",
+
+    message:
+        `Trung thu đầu tiên của em lại ở nơi xa, em cũng không biết là ngày gì, ở VN là em được thấy đèn lồng khắp nơi rồi, còn được xem múa lân nữa. Bên này ba mẹ toàn dắt em đi linh tinh công viên 
+        
+        Cả nhà đón trung thu dui dẻ nghen ♡🏮♡🏮♡🏮♡🏮♡`,
+
+    /*
+       Muốn ảnh cuối:
+
+       image: "images/final.jpg"
+    */
+
+    image: null
+
+};
